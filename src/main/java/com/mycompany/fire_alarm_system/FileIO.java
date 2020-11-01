fb6805c0a7ffbff376d93212ed027cdf308d8d21
smcri <poashan1234@gmail.com>
2020-11-01 20:23:41 +0530

    Adding Sensor And Object I/O classes

---
+++ src/main/java/com/mycompany/fire_alarm_system/FileIO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AADHARSH
+ */
+
+import java.io.*;
+
+public class FileIO {
+    
+    public String filepath;
+    
+    public void WriteObjectToFile(Object serObj) {
+ 
+        filepath = "D:\\FireAlarm\\src\\Projfolder\\Exercise17_03.dat";
+        
+        try {
+            File file = new File(filepath);
+            FileOutputStream fileOut = new FileOutputStream(filepath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(serObj);
+            objectOut.close();
+            System.out.println("The Object  was succesfully written to a file");
+ 
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void ReadObjectFromFile(Object serObj) {
+        
+        try{    
+            File file = new File(filepath);
+            FileInputStream fileIn = new FileInputStream(filepath);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            serObj = objectIn.readObject();
+            System.out.println(serObj);
+            Sensor s = (Sensor)serObj;
+            System.out.println(s.SensorType);
+            
+        } catch (Exception ex){
+            
+        }
+        
+    }
+    
+}
